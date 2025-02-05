8b54e6c014f51e64c78bd3e7ea841dca0a17e3f4
Eemka-a <mkacperczyk202@gmail.com>
2025-02-05 22:47:13 +0100

    dodanie funkcji policz

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+int policz(int a,int b) {
+    return a+b;
+}
 int main(void)
 {
-    printf("Hello, World!\n");
+    printf("%d + %d = %d\n",2,3,policz(2,3));
     return 0;
 }
