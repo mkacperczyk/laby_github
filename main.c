7f91f181037632a1cdfb526bee9fddd6d9e1c036
Eemka-a <mkacperczyk202@gmail.com>
2025-02-05 22:53:46 +0100

    funkcja policz zastapiona przez suma i dodanie funkcji roznica

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int suma(int a,int b) {
+    return a+b;
+}
+int roznica(int a,int b) {
+    return a-b;
+}
 int main(void)
 {
-    printf("Hello, World!\n");
+    printf("%d + %d = %d\n",3,2,suma(3,2));
+    printf("%d - %d = %d\n",3,2,roznica(3,2));
     return 0;
 }
